2ad3723add1c3a4dc792d531ae575179047d5cee
Sahand Ghaemi <sahandghaemi98@outlook.com>
2023-05-08 09:42:58 -0600

    update

--- FlatfileImport/Program.cs
+++ FlatfileImport/Program.cs
@@ -14,10 +14,13 @@ namespace FlatfileImport
         static void Main(string[] args)
         {
             // Replace the connection string values with your own values
-            string connectionString = "Data Source=myServerAddress;Initial Catalog=myDataBase;User Id=myUsername;Password=myPassword;";
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=QNXT_Test;Integrated Security=True";
+   
+           
 
             // Replace the file path with the path to your flat file
-            string filePath = @"C:\path\to\file.txt";
+            string filePath = @"C:\Users\963193\Documents\practice\QNXT_Training_Member_Inbound_PipeDelimited.txt";
+      
 
             // Open a connection to the database
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -33,40 +36,60 @@ namespace FlatfileImport
                     // Split the line into its individual fields
                     string[] fields = line.Split('|');
 
+                    string fileFormat = "FlatFile";
+
                     // Construct a SQL query that inserts the data into the database
-                    string query = "INSERT INTO Members (MemberId, FullName, DOB, Gender, Addr1, Addr2, City, Zip, SSN, CreateID, Createdate, Updateid, Lastupdate) " +
-               "VALUES (@MemberId, @FullName, @DOB, @Gender, @Addr1, @Addr2, @City, @Zip, @SSN, @CreateID, @Createdate, @Updateid, @Lastupdate)";
+                    string query = "INSERT INTO tzct_trn_member_import (MemberId, FullName, DOB, Gender, Addr1, Addr2, City, Zip, SSN, CreateID, Createdate, Updateid, Lastupdate,FileFormat) " +
+               "VALUES (@MemberId, @FullName, @DOB, @Gender, @Addr1, @Addr2, @City, @Zip, @SSN, @CreateID, @Createdate, @Updateid, @Lastupdate, @FileFormat)";
 
                     // Create a SqlCommand object to execute the query
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
                         // Concatenate the first name, last name, and middle name fields into the FullName field
-                        string fullName = string.Join(" ", new string[] { fields[1], fields[2], fields[3] });
-                        
-                        
-                        string gender = fields[5].Substring(0, 1);
+                        string fullName = string.Join(" ", new string[] { fields[0], fields[1], fields[2] });
+
+                        string memberId = GenerateMemberId();
+                    
+                        // skip all of the header row
+
+                        if (fields[0] == "FirstName")
+                        {
+                            continue;
+                        }
 
                         // Set the parameter values for the query
-                        command.Parameters.AddWithValue("@MemberId", fields[0]);
+                        command.Parameters.AddWithValue("@MemberId",memberId);
                         command.Parameters.AddWithValue("@FullName", fullName);
-                        command.Parameters.AddWithValue("@DOB", DateTime.Parse(fields[4]));
-                        command.Parameters.AddWithValue("@Gender", fields[5]);
+                        command.Parameters.AddWithValue("@DOB", DateTime.Parse(fields[3]));
+                        command.Parameters.AddWithValue("@Gender", fields[4].ToString().Substring(0, 1));
 
-                        command.Parameters.AddWithValue("@Addr1", fields[6]);
-                        command.Parameters.AddWithValue("@Addr2", fields[7]);
-                        command.Parameters.AddWithValue("@City", fields[8]);
-                        command.Parameters.AddWithValue("@Zip", fields[9]);
-                        command.Parameters.AddWithValue("@SSN", fields[10]);
+                        command.Parameters.AddWithValue("@Addr1", fields[5]);
+                        command.Parameters.AddWithValue("@Addr2", fields[6]);
+                        command.Parameters.AddWithValue("@City", fields[7]);
+                        command.Parameters.AddWithValue("@Zip", fields[8]);
+                        command.Parameters.AddWithValue("@SSN", fields[9]);
                         command.Parameters.AddWithValue("@CreateID", "DBO");
                         command.Parameters.AddWithValue("@Createdate", DateTime.Now);
                         command.Parameters.AddWithValue("@Updateid", "DBO");
                         command.Parameters.AddWithValue("@Lastupdate", DateTime.Now);
+                        command.Parameters.AddWithValue("@FileFormat", fileFormat);
+
                        
                         // Execute the query
                         command.ExecuteNonQuery();
                     }
+
                 }
+
+
             }
+            
+        }
+        private static string GenerateMemberId()
+        {
+
+            string memberId = Guid.NewGuid().ToString().Substring(0, 8);
+            return memberId;
         }
     }
 }
